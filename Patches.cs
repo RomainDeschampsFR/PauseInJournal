ab712728bfaae4bfc8b3f6f9bd169a37754d10e7
romainD <romain.deschamps61@sfr.fr>
2025-03-17 13:07:10 +0100

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+
+namespace PauseInJournal
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(Panel_Log), nameof(Panel_Log.Enable))]
+        internal class Panel_Log_Enable
+        {
+            private static void Postfix(Panel_Log __instance, bool enable)
+            {
+                if (enable)
+                {
+                    GameManager.m_GlobalTimeScale = 0;
+                }
+                else
+                {
+                    GameManager.m_GlobalTimeScale = 1;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using MelonLoader;
+using PauseInJournal;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PauseInJournal")]
+[assembly: AssemblyCopyright("Romain")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(PauseInJournal.Main), "PauseInJournal", "1.0.0", "Romain")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
